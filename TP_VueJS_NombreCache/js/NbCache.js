57abad767b2b3291fcd595766e56e66adeab7927
youssef <youssef.bannouni@hotmail.com>
2020-06-16 01:28:23 +0200

    correction bug du two way data binding , ajout de verification de la valeur soumise

--- TP_VueJS_NombreCache/js/NbCache.js
+++ TP_VueJS_NombreCache/js/NbCache.js
@@ -4,8 +4,7 @@ new Vue({
         valeurInc: 0,
         valeurSoumise :0,
         NbrTentativeRest :3,
-        CompNbr: "petit",
-        
+        CompNbr: "petit",     
         coupRate :false,
         reussite:false,
         abandon:false,
@@ -13,23 +12,22 @@ new Vue({
     },
     
     mounted:function(){
-        this.intialiserValeurInc();  /*intialiser la valeur inconue au demarage de la page*/ 
+        this.intialiserValeurInc();  /*initialiser la valeur inconnue au demarrage de la page*/ 
         
     },
     methods:{
         
 
-        intialiserValeurInc:function(){
+        intialiserValeurInc:function(){  /*creation aleatoire de la valeur inconnue */ 
         this.valeurInc=Math.round(Math.random() * 10); 
         console.log(this.valeurInc);
         },
         
-        rejouer:function(){       /*Re-intialiser tous les paramaitre*/   
+        rejouer:function(){       /*Re-intialiser tous les parametres*/   
         this.valeurInc=0;
         this.valeurSoumise=0;
         this.NbrTentativeRest=3;
-        this.CompNbr="petit";
-        
+        this.CompNbr="petit";       
         this.coupRate =false;
         this.reussite=false;
         this.abandon=false;
@@ -37,56 +35,59 @@ new Vue({
         this.intialiserValeurInc();
         },
 
-        abandonner:function(){
+        abandonner:function(){    /*Abondon de la partie*/ 
             this.abandon=true;
             this.coupRate=false;
             
 
         },
-        verifierValeur:function(){
+        verifierValeur:function(){ /*Comparaison de la valeur soumie avec la valeur inconne*/ 
             return (this.valeurSoumise-this.valeurInc);
        },
 
        jouer:function(){
-
-         
-  
-        if(this.NbrTentativeRest){    
-
-           if(!this.verifierValeur()){
-            this.reussite=true;
-            this.coupRate=false;
-            ;
-           }else if(this.verifierValeur()>0){
-            this.coupRate=true;
-            this.CompNbr="grand";
-           }else{
-            this.coupRate=true;
-            this.CompNbr="petit";
-          }
-       }else{
-        this.echec=true;
-        this.coupRate=false;
-        this.reussite=false;
-        }
-
-       this.NbrTentativeRest--; 
-       //resolution bug
-       if(this.NbrTentativeRest==0 && this.verifierValeur()){
-       	this.echec=true;
-        this.coupRate=false;
-        this.reussite=false;
         
-       }
-
-
+        //Verification de la validite de la valeur
+        if(this.$refs.maValeur.value=="" || !(this.$refs.maValeur.value>=0 && this.$refs.maValeur.value<=10))
+          alert("Merci de rentrer un nombre entre 0 et 10")
+        else{
+              //Recuperation de la valeur
+              this.valeurSoumise=this.$refs.maValeur.value;
+              //Verification que les tentatives ne sont pas encore epuisées
+              if(this.NbrTentativeRest){    
+                  //cas de valeur corretcte
+                  if(!this.verifierValeur()){
+                     this.reussite=true;
+                     this.coupRate=false;
+                     // cas de valeur superieure
+                  }else if(this.verifierValeur()>0){
+                     this.coupRate=true;
+                     this.CompNbr="grand";
+                  }else{ // cas de valeur inferieure
+                      this.coupRate=true;
+                      this.CompNbr="petit";
+                  }
+              }else{//cas de nombre tentative epuisée
+                 this.echec=true;
+                 this.coupRate=false;
+                this.reussite=false;
+              }
+              //Diminuer le comppteur de 1 pas apres chaque tentative
+              this.NbrTentativeRest--; 
+              /*resolution du bug de la derniere tentative , sinon apres 
+              3 mauvais coups , l'utilisateur aura droit à une 4 eme tentative*/
+              if(this.NbrTentativeRest==0 && this.verifierValeur()){
+              this.echec=true;
+              this.coupRate=false;
+              this.reussite=false;        
+              }
         } 
 
       
          
-      }
+      }//jouuer
         
-
+}
         
  
 });
